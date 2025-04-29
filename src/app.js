04eb58150023eba011b5aad837c561cd06354650
Nandini <nandini.bandreddi@conscendo.io>
2025-04-29 15:18:49 +0530

    added validation

--- src/app.js
+++ src/app.js
@@ -68,6 +68,26 @@ app.delete("/feed", async (req, res) => {
   }
 });
 
+// Update user by id
+app.patch("/feed", async (req, res) => {
+  try {
+    const { id } = req.query;
+    const updatedUser = req.body;
+    const user = await User.findByIdAndUpdate(id, updatedUser, {
+      runValidators: true,
+      new: true,
+    });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "User updated successfully", data: user });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error updating user", error: err.message });
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Connected to MongoDB");
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,23 +1,69 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: [3, "First name must be at least 3 characters"],
+      maxLength: [20, "First name is too long"],
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: [true, "Email already exists"],
+      validate(value) {
+        if (validator.isEmail(value)) return true;
+        return false;
+      },
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      validator(value) {
+        if (validator.isStrongPassword(value)) return true;
+        return false;
+      },
+    },
+    gender: {
+      type: String,
+      // validate(value) {
+      //   if (["male", "female", "other"].includes(value)) return true;
+      //   return false;
+      // },
+      lowercase: true,
+      enum: {
+        values: ["male", "female", "other"],
+        message: "{VALUE} is not supported",
+      },
+    },
+    habits: {
+      type: [String],
+      required: [true, "Habits are required"],
+
+      validate: {
+        validator: function (v) {
+          return v.length > 0 && v.length <= 3;
+        },
+      },
+    },
+    age: {
+      type: Number,
+      min: [18, "Age must be at least 18"],
+      max: [100, "Age is too high"],
+      required: function () {
+        return this.gender === "female" ? [true, "Age is required"] : false;
+      },
+    },
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("users", userSchema);
 export default User;
